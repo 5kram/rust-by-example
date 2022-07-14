b7f05fa6831603438d3ef3b2de2bb15ade360599
Markella <mkotti@existanze.com>
2022-07-14 11:44:01 +0300

    comments

---
+++ hello_world/comments.rs
@@ -0,0 +1,13 @@
+// Line comment which go to the end of the line
+// In general line comments are the recommended comment style
+
+fn main() {
+    /*
+     * Block comment which go to the closing delimiter
+     * Block comments are extremely useful for temporarily
+     * disabling chunks of code
+     */
+
+     let x = 3 + /* 9 + */ 7;
+     println!("x = {}", x);
+}
