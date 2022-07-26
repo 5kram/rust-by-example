ff20c3aaf432e957e5eec96c756a3162249bd477
Markella <mkotti@existanze.com>
2022-07-26 14:20:58 +0300

    file hierarchy

---
+++ modules/file_hierarchy/split.rs
@@ -0,0 +1,20 @@
+// Run: rustc modules/file_hierarchy/split.rs && ./split
+
+// This declaration will look for a file named `my.rs` and will
+// insert its contents inside a module named `my` under this scope
+mod my;
+
+fn function() {
+    println!("called `function()`");
+}
+
+fn main() {
+    my::function();
+
+    function();
+
+    my::indirect_access();
+
+    my::nested::function();
+}
+
