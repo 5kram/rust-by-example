2aaec2eeea0e5598758c11d22c6f1bac71df7193
Markella <mkotti@existanze.com>
2022-07-26 17:31:49 +0300

    extra binaries in the same project

---
+++ cargo/conventions/foo/src/bin/my_other_bin.rs
@@ -0,0 +1,6 @@
+// To tell cargo to compile or run this binary as opposed to the default 
+// or other binaries, we just pass cargo the --bin my_other_bin flag
+// $ cargo run --bin my_other_bin
+fn main() {
+    println!("Hello, world! from bin");
+}
